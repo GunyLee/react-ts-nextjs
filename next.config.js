8053892821a0388872f3dfaf60cbba2b22e5a4ea
GunyLee <lghjazz@gmail.com>
2022-03-01 00:25:03 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const debug = process.env.NODE_ENV !== 'production'
-const name = 'small-magic-project-deployment'
+const name = 'https://gunylee.github.io/react-ts-nextjs/'
 
 module.exports = {
   assetPrefix: !debug ? `/${name}/` : '',
